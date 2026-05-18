f41fb04af10d1de25b0dd6da33df478f36330294
Ruikai Liu <rickliu2000@outlook.com>
2026-05-18 16:40:43 +0800

    Initial rebase on qt 6.11.1

--- qt6-webengine/build.sh
+++ qt6-webengine/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+export CC=clang
+export CXX=clang++
 mkdir cmake-build
 cd cmake-build
-cmake .. -G Ninja -DBUILD_qtquick3dphysics=OFF -DQT_BUILD_EXAMPLES=ON
+cmake .. -G Ninja -DQT_BUILD_EXAMPLES=ON -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DBUILD_qtquick3dphysics=OFF
 ninja
